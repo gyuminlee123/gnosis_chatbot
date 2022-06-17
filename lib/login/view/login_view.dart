e9b4d9841d4147ae7d7a983b6b1674d0cc8865e1
gyuminlee123 <kekule81@gmail.com>
2022-06-17 18:28:46 +0900

    login page UI done

--- lib/login/view/login_view.dart
+++ lib/login/view/login_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gnosis_chatbot/repository/chatRepository.dart';
 import 'package:gnosis_chatbot/login/bloc/login_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gnosis_chatbot/login/widgets/custom_text_form.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,20 +12,72 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => LoginBloc(chatRepository: context.read<ChatRepository>()),
-      child: const LoginView(),
+      child: LoginView(),
     );
   }
 }
 
 class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
+
+  //입력 데이터를 얻어오기 위한 key와 controller들.
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login')),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(20.0),
+          children: [
+            SizedBox(height: 60),
+            //로그인 화면 로고
+            SvgPicture.asset(
+              'assets/chatbot.svg',
+              height: 200,
+              width: 200,
+            ),
+            const Text(
+              'Gnosis Prototype Bot',
+              style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold, ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            //username 입력
+            CustomTextFormField(text: "Username", type: inputStatus.name, controller: userNameController,),
+            const SizedBox(height: 15),
+            //Email 입력
+            CustomTextFormField(text: "Email", type: inputStatus.email, controller: emailController,),
+            const SizedBox(height: 50),
+            //Enter button
+            TextButton(
+              onPressed: () {
+                if(_formKey.currentState!.validate()) {
+                  print(userNameController.text);
+                  print(emailController.text);
+                }
+                else {
+                  print("Wrong values");
+                }
+              },
+              child: Text("Enter", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                primary: Colors.white,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),),
+                minimumSize: Size(400,60)
+              ),
+            ),
+          ],
+        ),
+
+      )
     );
   }
 }
+
+
 
 
---
+++ lib/login/widgets/custom_text_form.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+enum inputStatus { email, pass, name }
+
+class CustomTextFormField extends StatelessWidget {
+  CustomTextFormField(
+      {Key? key,
+      required this.text,
+      required this.type,
+      required this.controller})
+      : super(key: key);
+  final String text;
+  final inputStatus type;
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(text),
+        const SizedBox(height: 5.0),
+        TextFormField(
+          controller: controller,
+          validator: (value) {
+            print('$text validation check');
+            if (value!.isEmpty) {
+              return 'Please Enter $text';
+            }
+            if (type == inputStatus.email) {
+              RegExp regExp = RegExp(
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}'
+                  r'\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+              if (!regExp.hasMatch(value)) {
+                return 'Wrong Email address';
+              }
+            }
+            return null;
+          },
+          keyboardType: type == inputStatus.email
+              ? TextInputType.emailAddress
+              : TextInputType.name,
+          obscureText:
+              // 2. 해당 TextFormField가 비밀번호 입력 양식이면 **** 처리 해주기
+              type == inputStatus.pass ? true : false,
+          decoration: InputDecoration(
+            hintText: "Enter $text",
+            enabledBorder: OutlineInputBorder(
+              // 3. 기본 TextFormField 디자인
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedBorder: OutlineInputBorder(
+              // 4. 손가락 터치시 TextFormField 디자인
+              borderSide: const BorderSide(color: Colors.blue, width: 2.0),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            errorBorder: OutlineInputBorder(
+              // 5. 에러발생시 TextFormField 디자인
+              borderSide: const BorderSide(color: Colors.redAccent, width: 2.0),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              // 5. 에러가 발생 후 손가락을 터치했을 때 TextFormField 디자인
+              borderSide: const BorderSide(color: Colors.blue, width: 2.0),
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
