3e1837d0a5c29f0d137b8b82c0df2428d4a5e12f
gyuminlee123 <kekule81@gmail.com>
2022-06-23 17:38:12 +0900

    DONE -평가창 띄우는거 성공 할거 -평가내용 서버로 전송 -서버에서 기존 대화내용 가져오기 -기존 대화내용 지우기 기능

--- lib/chat/view/chat_view.dart
+++ lib/chat/view/chat_view.dart
@@ -13,8 +13,8 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) =>
-          ChatBloc(chatRepository: context.read<ChatRepository>())
-            ..add(const ChatInit()),
+      ChatBloc(chatRepository: context.read<ChatRepository>())
+        ..add(const ChatInit()),
       child: const ChatView(),
     );
   }
@@ -32,45 +32,106 @@ class _ChatViewState extends State<ChatView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('PROTO TYPE CHATTER BOT')),
-      body: BlocListener<ChatBloc, ChatState> (
+      body: BlocListener<ChatBloc, ChatState>(
         listenWhen: (previous, current) => previous.status != current.status,
         listener: (context, state) {
-          if(state.status == ChatStatus.ready) {
-            setState((){});
+          if (state.status == ChatStatus.ready) {
+            setState(() {});
           }
         },
         child: BlocBuilder<ChatBloc, ChatState>(
-          builder: (context, state) {
-            return Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      reverse: true,
-                      itemCount: state.messageList.length,
-                      itemBuilder: (context,index) {
-                        return ChatBubbles(
-                            state.messageList[index].message,
-                            state.messageList[index].isUser,
-                            state.messageList[index].name);
-                      },
+            builder: (context, state) {
+              return Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        reverse: true,
+                        itemCount: state.messageList.length,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () {
+                              showDialog(context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20) ),
+                                    title: Column(
+                                      children: [
+                                        const Text('Please Assess...',
+                                          style:TextStyle(fontSize:20, fontWeight: FontWeight.bold,)),
+                                        Text('\'${state.messageList[index].message}\'',
+                                          style:const TextStyle(fontSize:15)),
+                                        const Divider(thickness:1,color:Colors.grey),
+                                      ],
+                                    ),
+                                    content:
+                                        Column(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Text('\u{1F44C} Sensibleness'),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Text('\u{2705} Specificity'),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Text('\u{1F60D} Interesting'),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                Text('\u{1F480} Dangerous'),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                    actions: [
+                                      TextButton(
+                                        child: const Text('CANCEL'),
+                                        onPressed: () => Navigator.pop(context),
+                                      ),
+                                      TextButton(
+                                        child: const Text('OK'),
+                                        onPressed: () => Navigator.pop(context),
+                                      )
+                                    ]
+                                  );
+                                },
+                              );
+                            },
+                            child: ChatBubbles(
+                                state.messageList[index].message,
+                                state.messageList[index].isUser,
+                                state.messageList[index].name),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  NewMessage(
-                      isReady : (state.status == ChatStatus.ready),
-                      onSend: (message) {
-                        //message send button 눌렸을때 일처리를 여기서 한다.
-                        var newMessage = Message(isUser: true, name: state.username, time: DateTime.now(), message: message);
-                        state.messageList.insert(0,newMessage);
-                        setState((){
-                          print("setState1");
-                        });
-                        context.read<ChatBloc>().add(
-                          ChatSendMsg(message: message)
-                        );
-                      }),
-                ]
-            );
-          }
+                    NewMessage(
+                        isReady: (state.status == ChatStatus.ready),
+                        onSend: (message) {
+                          //message send button 눌렸을때 일처리를 여기서 한다.
+                          var newMessage = Message(isUser: true,
+                              name: state.username,
+                              time: DateTime.now(),
+                              message: message);
+                          state.messageList.insert(0, newMessage);
+                          setState(() {
+                            print("setState1");
+                          });
+                          context.read<ChatBloc>().add(
+                              ChatSendMsg(message: message)
+                          );
+                        }),
+                  ]
+              );
+            }
         ),
       ),
     );
